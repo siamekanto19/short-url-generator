04bb80d38470fa3e7fd4ff8214000d104a989172
Siam <siamekanto360@gmail.com>
2024-11-21 06:21:20 +0600

    update next config

--- app/[shortUrl]/page.tsx
+++ app/[shortUrl]/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
 import { fetchOriginalUrl } from '@/server/requests'
+import { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
-const ShortUrlPage = async ({ params }: { params: Record<string, string> }) => {
+const ShortUrlPage: NextPage<PageProps> = async ({ params }) => {
   const originalUrl = await fetchOriginalUrl(params.shortUrl)
 
   if (originalUrl) {
